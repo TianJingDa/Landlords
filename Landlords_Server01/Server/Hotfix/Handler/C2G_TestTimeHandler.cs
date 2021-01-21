f4960b54a1132fbae646598e005c434a38063eea
Tian Jingda <5304654@sina.com>
2021-01-21 10:08:17 +0800

    Init

---
+++ Landlords_Server01/Server/Hotfix/Handler/C2G_TestTimeHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using ETModel;
+
+namespace ETHotfix
+{
+    [MessageHandler(AppType.Gate)]
+    public class C2G_TestTimeHandler : AMRpcHandler<C2G_TestTime, G2C_TestTime>
+    {
+        protected override async ETTask Run(Session session, C2G_TestTime request, G2C_TestTime response, Action reply)
+        {
+            response.Message = DateTime.Now.ToString();
+            reply();
+        }
+    }
+}
