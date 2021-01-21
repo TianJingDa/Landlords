c906ef03d8e3407b78ecd2e6010bbf7d72852ded
Tian Jingda <5304654@sina.com>
2021-01-21 15:49:51 +0800

    增加消息

---
+++ Landlords_Client01/Unity/Assets/Model/Landlords/Handler/G2C_TestHotfixMessageHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ETModel
+{
+    [MessageHandler(AppType.ClientM)]
+    public class G2C_TestHotfixMessageHandler : AMHandler<G2C_TestHotfixMessage>
+    {
+        protected override async ETTask Run(Session session, G2C_TestHotfixMessage message)
+        {
+            Log.Info(message.Info);
+        }
+    }
+}
